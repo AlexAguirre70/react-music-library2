bf8e8facdcbb8a19bd49efb0520f8a4d047280d3
Alex Aguirre <teamaguirre@gmail.com>
2022-09-18 18:56:04 -0700

    Rendered Music lists via API call

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ import './App.css'
 
 function App(){
     let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
+    let [message, setMessage] = useState(`Search for ${search} Music!`)
     let [data, setData] = useState([])
 
+    const API_URL = 'https://itunes.apple.com/search?term='
+  useEffect(()=>{
+    if (search) {
+    const fetchData = async () =>{
+      document.title = `${search} Music`
+      const response = await fetch(API_URL + search)
+      console.log(API_URL+search)
+      const resData = await response.json()
+      if (resData.results.length >0){setData(resData.results)} else {setMessage('Music Not Found')}
+    }
+    fetchData()
+  }
+  }, [search])  
+  const handleSearch = (e,term)=>{
+    e.preventDefault ()
+    setSearch(term)
+  }
     return (
         <div className='App'>
-            <SearchBar />
+            <SearchBar handleSearch={handleSearch} />
             {message}
-            <Gallery />
+            <Gallery data={data}/>
         </div>
     )
 }
