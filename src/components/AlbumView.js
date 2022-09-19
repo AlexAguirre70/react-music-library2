515329792451d99249958ef687b8ad6f9c5957f3
Alex Aguirre <teamaguirre@gmail.com>
2022-09-19 16:57:47 -0700

    add the artist view dynamic data fetch functionality

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -6,9 +6,18 @@ import { useParams } from 'react-router-dom'
 function AlbumView() {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
-
+    useEffect (()=> {
+        const API_URL = `http://localhost:3000/artist/${id}`
+        const fetchData = async ()=>{
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            return(resData)
+        }
+        fetchData()
+    })
     return (
         <div>
+            
             <h2>The id passed was: {id}</h2>
             <p>Album Data Goes Here!</p>
         </div>
